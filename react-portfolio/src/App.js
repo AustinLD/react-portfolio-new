12325e9972746384c5e6f11bf1344b1d408b2071
Austin Donovan <AustinLDonovan@gmail.com>
2022-06-05 13:09:49 -0400

    About me folder created and styled with filler text

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,13 +1,15 @@
 import './App.scss';
 import { Routes, Route } from "react-router-dom"
 // import Layout from "./components/Layout"
-import Home from './components/Home';
+import Home from "./components/Home";
+import About from "./components/About"
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </>
   )
---
+++ react-portfolio/src/components/About/index.js
@@ -0,0 +1,23 @@
+import "./index.scss";
+import Layout from "../Layout"
+
+const About = () => {
+  return (
+    <>
+    <Layout />
+    <div className="container about-page">
+      <div className="text-zone">
+        <h1>
+          About Me
+        </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget ipsum fringilla, auctor erat eget, cursus enim. Donec iaculis vel lorem at maximus. Duis varius facilisis rhoncus. Integer ornare fermentum tortor eu bibendum. Proin mauris ipsum, suscipit id cursus id, convallis vel leo. In hac habitasse platea dictumst. Praesent </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget ipsum fringilla, auctor erat eget, cursus enim. Donec iaculis vel lorem at maximus. Duis varius . Ut id egestas quam. Duis volutpat molestie enim et faucibus. Nulla non turpis lacinia, tempus odio venenatis, dapibus nibh.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget ipsum fringilla, auctor erat eget, cursus enim. Donec iaculis vel lorem at maximus. Duis varius  estas quam. Duis volutpat molestie enim et faucibus. Nulla non turpis lacinia, tempus odio venenatis, dapibus nibh.</p>
+      </div>
+    </div>
+
+    </>
+  )
+}
+
+export default About
